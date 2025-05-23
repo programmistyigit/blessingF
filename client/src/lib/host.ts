808314d45e668674bd09dc871786a27f1c02557b
pragrammistyigit <pragrammistyigit@gmail.com>
2025-05-23 23:33:05 +0500

    hid

--- client/src/lib/host.ts
+++ client/src/lib/host.ts
@@ -1,4 +1,2 @@
 // API serverning host manzili
-export const baseHost = process.env.NODE_ENV === 'production' 
-  ? window.location.origin  // Production muhitida aynan shu sayt manzilidan olinadi
-  : "http://localhost:8000"; // Development muhitida local server
+export const baseHost = "http://api.umid-blessing.uz";
